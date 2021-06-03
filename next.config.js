e822430d06f237097915ecde5d7629ade7cd0036
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-06-03 17:20:23 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const { default: loadConfig } = require("next/dist/next-server/server/config")
-const path = require('path');
-
-module.exports = {
-    webpack: (config) => {
-         config.resolve.alias['@'] = path.resolve(__dirname);
-         return config;
-    }
-}
